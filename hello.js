a5ab45b42529bb00ccb1b10f8cde45f08777f470
nurmohammadweb <nurmohammad11266@gmail.com>
2025-08-29 18:02:49 +0600

    added function first section

--- hello.js
+++ hello.js
@@ -1,1 +1,10 @@
-console.log()
+const coinEl = document.getElementById("coin-point");
+const heartEl = document.getElementById("heart-number");
+const copeEl = document.getElementById("cope-numper");
+const cardContainer = document.getElementById("card-container");
+const historyAside = document.getElementById("aside-container");
+const clearBtn = document.getElementById("clear-btn");
+
+function getInt(el) {
+  return parseInt(el.textContent.trim(), 10) || 0;
+}
